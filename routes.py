14b57dcde27a672c8d4175cd24433fa33fdc1083
Michael Wang <michael.mhw1@gmail.com>
2019-03-06 16:57:33 +1100

    added frontend files

--- routes.py
+++ routes.py
@@ -2,3 +2,6 @@ from server import app, system
 from flask import redirect, request, render_template, url_for, abort
 
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return(render_template('home.html'))
