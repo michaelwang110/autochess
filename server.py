33f2f7a0ce780812987dd97cb99cab37c29e6014
Michael Wang <michael.mhw1@gmail.com>
2019-04-01 03:11:55 +1100

    modified csv input

--- server.py
+++ server.py
@@ -4,16 +4,17 @@ from src.Piece import Piece
 import csv
 
 app = Flask(__name__)
-app.secret_key = "very-secret-123"
+app.secret_key = 'very-secret-123'
 
 # Board setup
 board = BoardSystem()
 
+# input Piece details
 with open('pieces/pieces.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         name = row[0]
-        species = [row[1], row[2]]
+        species = [row[1], None if row[2] == 'None' else row[2]]
         clas = row[3]
         cost = int(row[4])
         tier = int(row[5])
