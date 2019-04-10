ee167fafd873c3e56342aa00d6b8fafa0b03f592
Michael Wang <michael.mhw1@gmail.com>
2019-04-10 21:18:08 +1000

    implementing next move algo

---
+++ src/algo/algo.py
@@ -0,0 +1,13 @@
+def algo(pieces_on_board, piece_dict):
+    # 1 gold
+    if len(pieces_on_board) == 0:
+    # 1, 2 gold
+    elif len(pieces_one_board) <= 1:
+    # 1, 2, 3 gold
+    elif len(pieces_on_board) <= 3:
+    # 1, 2, 3, 4 gold
+    elif len(pieces_on_board) <= 6:
+    # 1, 2, 3, 4, 5 gold:
+    else:
+        
+    return pieces_ranking
